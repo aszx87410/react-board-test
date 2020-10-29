459b581996633c79dc72d28c5784b88980fedc77
huli <aszx87410@gmail.com>
2020-10-29 20:18:03 +0800

    add prettier

---
+++ src/TodoItem.js
@@ -0,0 +1,92 @@
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { MEDIA_QUERY_MD, MEDIA_QUERY_LG } from "./constants/style";
+
+const TodoItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+  border: 1px solid black;
+
+  & + & {
+    margin-top: 12px;
+  }
+`;
+
+const TodoContent = styled.div`
+  color: ${(props) => props.theme.colors.primary_300};
+  font-size: 12px;
+
+  ${(props) =>
+    props.size === "XL" &&
+    `
+    font-size: 20px;
+  `}
+
+  ${(props) =>
+    props.$isDone &&
+    `
+    text-decoration: line-through;
+  `}
+`;
+
+const TodoButtonWrapper = styled.div``;
+
+const Button = styled.button`
+  padding: 4px;
+  color: black;
+  font-size: 20px;
+
+  ${MEDIA_QUERY_MD} {
+    font-size: 16px;
+  }
+
+  ${MEDIA_QUERY_LG} {
+    font-size: 16px;
+  }
+
+  &:hover {
+    color: red;
+  }
+
+  & + & {
+    margin-left: 4px;
+  }
+`;
+
+const RedButton = styled(Button)`
+  color: red;
+`;
+
+export default function TodoItem({
+  className,
+  size,
+  todo,
+  handleDeleteTodo,
+  handleToggleIsDone,
+}) {
+  const handleTogglerClick = () => {
+    handleToggleIsDone(todo.id);
+  };
+
+  const handleDeleteClick = () => {
+    handleDeleteTodo(todo.id);
+  };
+
+  return (
+    <TodoItemWrapper className={className} data-todo-id={todo.id}>
+      <TodoContent $isDone={todo.isDone} size={size}>
+        {todo.content}
+      </TodoContent>
+      <TodoButtonWrapper>
+        <Button onClick={handleTogglerClick}>
+          {todo.isDone && "未完成"}
+          {!todo.isDone && "已完成"}
+        </Button>
+        <RedButton onClick={handleDeleteClick}>刪除</RedButton>
+      </TodoButtonWrapper>
+    </TodoItemWrapper>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary_300: "#3333dd",
+    primary_400: "#e33e3e",
+    primary_500: "#af0505",
+  },
+};
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
